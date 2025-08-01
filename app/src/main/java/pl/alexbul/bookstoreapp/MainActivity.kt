8dbbcf52519a9fd067e3f106b2508fb78f1ca2f6
Admin <raccoonsaf@rambler.ru>
2025-08-01 22:29:17 +0300

    Health check

--- app/src/main/java/pl/alexbul/bookstoreapp/MainActivity.kt
+++ app/src/main/java/pl/alexbul/bookstoreapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package pl.alexbul.bookstoreapp
 
-import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,7 +17,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -23,10 +24,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -36,9 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.google.firebase.Firebase
-import com.google.firebase.database.core.Context
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.storage
 import pl.alexbul.bookstoreapp.data.Book
@@ -48,9 +45,28 @@ import java.io.ByteArrayOutputStream
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      //  enableEdgeToEdge()
+        //  enableEdgeToEdge()
         setContent {
-           MainScreen()
+            val fb = Firebase.firestore
+            val fs = Firebase.storage.reference.child("images")
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.PickVisualMedia()
+            ) { uri ->
+                if (uri == null) return@rememberLauncherForActivityResult
+                val task = fs.child("cat.jpg").putBytes(bitmapToByteArrayFromPhone(this, uri))
+                task.addOnSuccessListener { uploadTask ->
+                    uploadTask.metadata?.reference?.downloadUrl?.addOnCompleteListener { uriTask ->
+                        saveBook(fb, uriTask.result.toString())
+                    }
+                }
+            }
+            MainScreen {
+                launcher.launch(
+                    PickVisualMediaRequest(
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            }
         }
     }
 }
@@ -64,7 +80,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun MainScreen() {
+fun MainScreen(OnClick: () -> Unit) {
     val context = LocalContext.current
     val fb = Firebase.firestore
     val fs = Firebase.storage.reference.child("images")
@@ -94,7 +110,7 @@ fun MainScreen() {
                 .fillMaxHeight(0.8f)
         )
         {
-            items(list.value){ book ->
+            items(list.value) { book ->
                 Card(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -105,11 +121,13 @@ fun MainScreen() {
                         modifier = Modifier.fillMaxWidth(),
                         verticalAlignment = Alignment.CenterVertically
 
-                       ) {
+                    ) {
                         AsyncImage(
                             model = book.imageUrl,
                             contentDescription = null,
-                            modifier = Modifier.height(70.dp).width(70.dp)
+                            modifier = Modifier
+                                .height(70.dp)
+                                .width(70.dp)
                         )
 
 
@@ -133,51 +151,60 @@ fun MainScreen() {
             onClick = {
 
                 val task = fs.child("cat.jpg").putBytes(bitmapToByteArray(context))
-task.addOnSuccessListener {
-        uploadTask ->
-    uploadTask.metadata?.reference?.downloadUrl?.addOnCompleteListener {uriTask ->
-        saveBook(fb, uriTask.result.toString() )}
-}
+                task.addOnSuccessListener { uploadTask ->
+                    uploadTask.metadata?.reference?.downloadUrl?.addOnCompleteListener { uriTask ->
+                        saveBook(fb, uriTask.result.toString())
+                    }
+                }
             }) {
             Text(
-            text = "Add book",
-          //  modifier = modifier
-        )}
+                text = "Add book",
+                //  modifier = modifier
+            )
+        }
         Button(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(10.dp),
             onClick = {
 
-                val task = fs.child("cat.jpg").putBytes(bitmapToByteArray(context))
-                task.addOnSuccessListener {
-                        uploadTask ->
-                    uploadTask.metadata?.reference?.downloadUrl?.addOnCompleteListener {uriTask ->
-                        saveBook(fb, uriTask.result.toString() )}
-                }
+
+                OnClick()
             }) {
             Text(
                 text = "Open Photo",
                 //  modifier = modifier
-            )}
+            )
+        }
     }
 
 }
 
-private fun bitmapToByteArray( context: android.content.Context): ByteArray   {
-     val bitMap = BitmapFactory.decodeResource(context.resources, R.drawable.cat)
-val boas = ByteArrayOutputStream()
+private fun bitmapToByteArray(context: android.content.Context): ByteArray {
+    val bitMap = BitmapFactory.decodeResource(context.resources, R.drawable.cat)
+    val boas = ByteArrayOutputStream()
     bitMap.compress(Bitmap.CompressFormat.JPEG, 100, boas)
     return boas.toByteArray()
 }
 
-private fun saveBook (fb: FirebaseFirestore, url: String){
-    fb.collection("books").document().set(Book (
-        "Raccon",
-        "raccoon",
-        "900",
-        "true",
-        url),)
+private fun bitmapToByteArrayFromPhone(context: android.content.Context, uri: Uri): ByteArray {
+    val inputStream = context.contentResolver.openInputStream(uri)
+    val bitMap = BitmapFactory.decodeStream(inputStream)
+    val boas = ByteArrayOutputStream()
+    bitMap.compress(Bitmap.CompressFormat.JPEG, 100, boas)
+    return boas.toByteArray()
+}
+
+private fun saveBook(fb: FirebaseFirestore, url: String) {
+    fb.collection("books").document().set(
+        Book(
+            "Raccon",
+            "raccoon",
+            "900",
+            "true",
+            url
+        ),
+    )
 
 }
 
